e52ddc0b6ba6be10925fe10918507e924b8dff72
kkawamura <kkawamura@logos.t.u-tokyo.ac.jp>
2019-03-29 00:32:02 +0900

    add wsl mozc settings

--- zshrc_local_settings/zshrc_local_in_wsl.zsh
+++ zshrc_local_settings/zshrc_local_in_wsl.zsh
@@ -3,4 +3,13 @@
 ## See also: https://qiita.com/kilo/items/c8b51f2b52bf5c6f3aa3
 export DISPLAY=:0.0
 export GDK_SCALE=2
-
+## Han-Zen settings
+## See: https://qiita.com/dozo/items/97ac6c80f4cd13b84558
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+export DefaultIMModule=fcitx
+if [[ ${SHLVL:-} = 1 ]]; then
+  xset -r 49  > /dev/null 2>&1
+  (fcitx-autostart > /dev/null 2>&1 &)
+fi
